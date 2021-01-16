3264927ddb85f42fa4b0359c1856f8085e02064a
Dominik277 <dominikdoric277@gmail.com>
2021-01-16 15:21:18 +0100

    Kreirana logika za FABActivity da u Toast-u prikazuje ono sto smo upisali u polja za EditText.

--- app/src/main/java/hr/dominik/library/draweraktivnosti/FABActivity.java
+++ app/src/main/java/hr/dominik/library/draweraktivnosti/FABActivity.java
@@ -2,10 +2,19 @@ package hr.dominik.library.draweraktivnosti;
 
 import androidx.appcompat.app.AppCompatActivity;
 import hr.dominik.library.R;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class FABActivity extends AppCompatActivity {
+
+    Button gumbPosaljiUpit;
+    EditText editTextIme;
+    EditText editTextPrezime;
+    EditText editTextEmail;
+    EditText editTextUpit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +22,23 @@ public class FABActivity extends AppCompatActivity {
         setContentView(R.layout.activity_f_a_b);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        gumbPosaljiUpit = findViewById(R.id.gumbPosaljiUpit);
+        editTextIme = findViewById(R.id.editTextIme);
+        editTextPrezime = findViewById(R.id.editTextPrezime);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextUpit = findViewById(R.id.editTextUpit);
+
+        gumbPosaljiUpit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ime = editTextIme.getText().toString();
+                String prezime = editTextPrezime.getText().toString();
+                String email = editTextEmail.getText().toString();
+                Toast.makeText(FABActivity.this,ime + " " + prezime + " " +
+                        "vaš upit je zaprimljen i bit će ubrzo odgovoren na " + email,Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
