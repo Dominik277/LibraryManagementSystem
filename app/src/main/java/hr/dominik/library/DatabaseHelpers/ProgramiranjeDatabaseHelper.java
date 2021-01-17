38db68c2fbd48ae0cacc7cf61724863997bebd32
Dominik277 <dominikdoric277@gmail.com>
2021-01-17 16:04:24 +0100

    Kreirana baza podataka i tablica unutar baze podataka unutar metode onCreate().

--- app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
+++ app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
@@ -8,18 +8,38 @@ import androidx.annotation.Nullable;
 
 public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DB_NAME = "programiranje";
+    private static final int DB_VERSION = 1;
 
-    public ProgramiranjeDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public ProgramiranjeDatabaseHelper(@Nullable Context context) {
+        super(context,DB_NAME,null,DB_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("CREATE TABLE PROGRAMIRANJE ("
+                + "_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+                + "IME_KNJIGE TEXT, "
+                + "PISAC TEXT, "
+                + "STRANICE TEXT, "
+                + "GODINA TEXT, "
+                + "SLIKA INTEGER);");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
     }
+
+    private static void insertProgramiranje(SQLiteDatabase db,
+                                            String imeKnjige,
+                                            String pisac,
+                                            String stranice,
+                                            String godina,
+                                            int slika){
+
+
+
+    }
+
 }
