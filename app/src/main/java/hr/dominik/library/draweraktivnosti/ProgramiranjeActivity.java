8959259ece0172f8b4b345c328f351ed56bd0aa8
Dominik277 <dominikdoric277@gmail.com>
2021-01-17 20:09:50 +0100

    Jos jedan neuspjeli pokusaj.

--- app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
+++ app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
@@ -1,21 +1,30 @@
 package hr.dominik.library.draweraktivnosti;
 
 import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.DatabaseHelpers.ProgramiranjeDatabaseHelper;
 import hr.dominik.library.ProgramiranjeAdapter;
 import hr.dominik.library.ProgramiranjeKnjige;
 import hr.dominik.library.R;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class ProgramiranjeActivity extends AppCompatActivity {
 
     GridView gridView;
+    private SQLiteDatabase db;
+    private Cursor cursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +33,25 @@ public class ProgramiranjeActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         gridView = findViewById(R.id.gridView);
 
+        SQLiteOpenHelper programiranjeDatabaseHelper = new ProgramiranjeDatabaseHelper(this);
+        try {
+            db = programiranjeDatabaseHelper.getReadableDatabase();
+            cursor = db.query("PROGRAMIRANJE",
+                             new String[]{"_id","IME_KNJIGE","PISAC","STRANICE","GODINA","SLIKA"}
+                             ,null,null,null,null,null);
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this,
+                    android.R.layout.simple_list_item_1,
+                    cursor,
+                    new String[]{"IME_KNJIGE"},
+                    new int[]{android.R.id.text1},
+                    0);
+        }catch (SQLException e){
+            Toast toast = Toast.makeText(this,"Database unavailable",Toast.LENGTH_LONG);
+            toast.show();
+        }
 
-        ArrayList<ProgramiranjeKnjige> courseModelArrayList = new ArrayList<ProgramiranjeKnjige>();
+/*
+        ArrayList<ProgramiranjeKnjige> courseModelArrayList = new ArrayList<>();
         courseModelArrayList.add(new ProgramiranjeKnjige("Naučite React:Prevod Drugog Izdanja","Pisac:Kirupa Činatambi","Stranice:298","Godina:2018.", R.drawable.react));
         courseModelArrayList.add(new ProgramiranjeKnjige("Arduino:Uvod u programiranje","Pisac:Simon Monk","Stranice:194","Godina:2017", R.drawable.arduino));
         courseModelArrayList.add(new ProgramiranjeKnjige("Demistificirani C++","Pisac:Julijan Šribar","Stranice:1112","Godina:2018", R.drawable.demistrificiranic));
@@ -38,5 +64,13 @@ public class ProgramiranjeActivity extends AppCompatActivity {
         ProgramiranjeAdapter adapter = new ProgramiranjeAdapter(this, courseModelArrayList);
         gridView.setAdapter(adapter);
 
+ */
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        cursor.close();
+        db.close();
     }
 }
