43949aa6e05d257727cf1bfc8fb0637f37af8373
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 21:46:01 +0100

    Kreirano da prilikom klika na neki item ListView-a se pokrene nova aktivnos ako zelimo azurirati ili izbrisati item.

--- app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
+++ app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
@@ -10,9 +10,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -35,6 +38,27 @@ public class ProgramiranjeActivity extends AppCompatActivity {
                                     ,new String[]{"name","author","pages"}
                                     ,new int[]{R.id.textViewName,R.id.textViewAuthor,R.id.textViewPages});
         listView.setAdapter(adapter);
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView textViewName = view.findViewById(R.id.textViewName);
+                TextView textViewAutor = view.findViewById(R.id.textViewAuthor);
+                TextView textViewStranice = view.findViewById(R.id.textViewPages);
+
+                String name = textViewName.getText().toString();
+                String autor = textViewAutor.getText().toString();
+                String stranice = textViewStranice.getText().toString();
+
+                Intent intent = new Intent(getApplicationContext(),ModifyActivity.class);
+                intent.putExtra("name",name);
+                intent.putExtra("author",autor);
+                intent.putExtra("pages",stranice);
+
+                startActivity(intent);
+                return true;
+            }
+        });
 
     }
 
