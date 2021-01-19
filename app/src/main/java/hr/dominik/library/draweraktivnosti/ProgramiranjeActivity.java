7a390b2b3d5fb21b37aed0b119f3010a146518bf
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 18:38:34 +0100

    Kreiran menu u ProgramiranjeActivity tako da se prilikom klika otvara nova aktivnost koja sluzi za upisivanje nove knjige u bazu podataka.

--- app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
+++ app/src/main/java/hr/dominik/library/draweraktivnosti/ProgramiranjeActivity.java
@@ -1,9 +1,14 @@
 package hr.dominik.library.draweraktivnosti;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import hr.dominik.library.R;
+import hr.dominik.library.UnesiteKnjiguActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ProgramiranjeActivity extends AppCompatActivity {
 
@@ -17,5 +22,17 @@ public class ProgramiranjeActivity extends AppCompatActivity {
 
 
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.programiranje_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        startActivity(new Intent(ProgramiranjeActivity.this, UnesiteKnjiguActivity.class));
+        return true;
     }
 }
