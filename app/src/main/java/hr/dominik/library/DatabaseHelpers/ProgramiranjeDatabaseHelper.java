9243015e46153ba3ebb6e56ddb6522955ee79fb5
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 18:12:51 +0100

    Implementiran kod za kreiranje baze podataka i dodane CRUD metode koje jos moraju biti dorađene.

--- app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
+++ app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
@@ -1,26 +1,52 @@
 package hr.dominik.library.DatabaseHelpers;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 
-
 public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
 
+    private static final String DATABASE_NAME = "programiranje_db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String TABLE_NAME = "programiranje";
+    private static final String KEY_ID = "id";
+    private static final String KEY_NAME = "name";
 
-    public ProgramiranjeDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(context, name, factory, version);
+    public ProgramiranjeDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
+                + KEY_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
+                + KEY_NAME + "TEXT" + ")";
+        db.execSQL(CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
+        onCreate(db);
+    }
+
+    public void insertBook(String name){
 
     }
+
+    public Cursor getBook(){
+
+    }
+
+    public int updateBook(long id,String name){
+
+    }
+
+    public void deleteRow(long id){
+
+    }
+
 }
