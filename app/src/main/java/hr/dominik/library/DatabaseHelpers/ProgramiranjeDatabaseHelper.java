69c93484e7a62ef5e15f130927e75071fd1afbbc
Dominik277 <dominikdoric277@gmail.com>
2021-01-19 19:45:07 +0100

    Napravljene sitne preinake u CRUD operacijama.

--- app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
+++ app/src/main/java/hr/dominik/library/DatabaseHelpers/ProgramiranjeDatabaseHelper.java
@@ -7,6 +7,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.view.View;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 
 public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
 
@@ -15,6 +18,8 @@ public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
     public static final String TABLE_NAME = "programiranje";
     public static final String KEY_ID = "id";
     public static final String KEY_NAME = "name";
+    public static final String KEY_AUTHOR = "author";
+    public static final String KEY_PAGES = "pages";
 
     SQLiteDatabase database;
 
@@ -26,7 +31,10 @@ public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
     public void onCreate(SQLiteDatabase db) {
         String CREATE_TABLE = "CREATE TABLE " + TABLE_NAME + "("
                 + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + KEY_NAME + " TEXT " + ")";
+                + KEY_NAME + " TEXT, "
+                + KEY_AUTHOR + " TEXT, "
+                + KEY_PAGES + " TEXT "
+                + ")";
         db.execSQL(CREATE_TABLE);
     }
 
@@ -36,25 +44,29 @@ public class ProgramiranjeDatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public void insertBook(String name){
+    public void insertBook(String name,String author,String pages){
         database = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(ProgramiranjeDatabaseHelper.KEY_NAME,name);
+        contentValues.put(KEY_NAME,name);
+        contentValues.put(KEY_AUTHOR,author);
+        contentValues.put(KEY_PAGES,pages);
         database.insert(TABLE_NAME,null,contentValues);
         database.close();
     }
 
-    public Cursor getBook(){
-        database = this.getReadableDatabase();
-        String[] columns = new String[]{ProgramiranjeDatabaseHelper.KEY_ID,
-                                        ProgramiranjeDatabaseHelper.KEY_NAME};
-        Cursor cursor = database.query(ProgramiranjeDatabaseHelper.TABLE_NAME
-                        ,columns
-                        ,null,null,null,null,null);
-        if (columns != null){
-            cursor.moveToFirst();
+    public ArrayList<HashMap<String,String>> getBooks(){
+        SQLiteDatabase database = this.getWritableDatabase();
+        ArrayList<HashMap<String,String>> booksList = new ArrayList<>();
+        String query = " SELECT name, author, pages FROM " +TABLE_NAME;
+        Cursor cursor = database.rawQuery(query,null);
+        while (cursor.moveToNext()){
+            HashMap<String,String> books = new HashMap<>();
+            books.put("name",cursor.getString(cursor.getColumnIndex(KEY_NAME)));
+            books.put("author",cursor.getString(cursor.getColumnIndex(KEY_AUTHOR)));
+            books.put("pages",cursor.getString(cursor.getColumnIndex(KEY_PAGES)));
+            booksList.add(books);
         }
-        return cursor;
+        return booksList;
     }
 
     public int updateBook(long id,String name){
