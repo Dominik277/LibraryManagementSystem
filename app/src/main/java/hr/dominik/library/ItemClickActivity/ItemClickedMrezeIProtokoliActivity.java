9bb84390357d0ce2e194942a55e0b6a05211a242
Dominik277 <dominikdoric277@gmail.com>
2021-01-20 17:40:30 +0100

    Dodane sve aktivnosti koje ce se otvarati kada se pritisne neki item u listView-u.

---
+++ app/src/main/java/hr/dominik/library/ItemClickActivity/ItemClickedMrezeIProtokoliActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.ItemClickActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class ItemClickedMrezeIProtokoliActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_clicked_mreze_i_protokoli);
+    }
+}
---
+++ app/src/main/java/hr/dominik/library/ItemClickActivity/ItemClickedOperativniSustaviActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.ItemClickActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class ItemClickedOperativniSustaviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_clicked_operativni_sustavi);
+    }
+}
