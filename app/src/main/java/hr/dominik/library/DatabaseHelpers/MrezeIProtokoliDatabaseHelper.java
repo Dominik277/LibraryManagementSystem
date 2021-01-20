75a73e7448abf45b2c84712693597fe772d2bc15
Dominik277 <dominikdoric277@gmail.com>
2021-01-20 17:22:16 +0100

    Dodani SQLite helperi za tri preostale klase koje cemo koristiti u aplikaciji.

---
+++ app/src/main/java/hr/dominik/library/DatabaseHelpers/MrezeIProtokoliDatabaseHelper.java
@@ -0,0 +1,25 @@
+package hr.dominik.library.DatabaseHelpers;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MrezeIProtokoliDatabaseHelper extends SQLiteOpenHelper {
+
+
+    public MrezeIProtokoliDatabaseHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
