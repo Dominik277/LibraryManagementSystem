ab34b98a3fc63eee41a02712c47b03bc47f3d62e
Dominik277 <dominikdoric277@gmail.com>
2021-01-21 23:28:10 +0100

    Dodane ostale aktivnosti unutar kojih ce se moci traziti pojedina knjiga.

---
+++ app/src/main/java/hr/dominik/library/SearchActivity/SearchHardverActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.SearchActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class SearchHardverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_hardver);
+    }
+}
---
+++ app/src/main/java/hr/dominik/library/SearchActivity/SearchOperativniSustaviActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.SearchActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class SearchOperativniSustaviActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_operativni_sustavi);
+    }
+}
