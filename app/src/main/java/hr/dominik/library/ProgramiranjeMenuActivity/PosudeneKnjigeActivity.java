3b065799c85def6e354760cfd6e778cfe0dc10ab
Dominik277 <dominikdoric277@gmail.com>
2021-01-22 20:36:19 +0100

    Dodane aktivnosti za Posuđene knjige i za WishList.

---
+++ app/src/main/java/hr/dominik/library/ProgramiranjeMenuActivity/PosudeneKnjigeActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.ProgramiranjeMenuActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class PosudeneKnjigeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_posudene_knjige);
+    }
+}
---
+++ app/src/main/java/hr/dominik/library/ProgramiranjeMenuActivity/WishListActivity.java
@@ -0,0 +1,15 @@
+package hr.dominik.library.ProgramiranjeMenuActivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+import hr.dominik.library.R;
+
+import android.os.Bundle;
+
+public class WishListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wish_list);
+    }
+}
