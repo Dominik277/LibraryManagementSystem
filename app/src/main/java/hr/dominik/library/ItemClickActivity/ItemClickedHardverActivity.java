1fe0b7408a70f542cd86a18c9c8d5f74e4a9693a
Dominik277 <dominikdoric277@gmail.com>
2021-01-23 01:18:47 +0100

    Dodano dosta sitnica.

--- app/src/main/java/hr/dominik/library/ItemClickActivity/ItemClickedHardverActivity.java
+++ app/src/main/java/hr/dominik/library/ItemClickActivity/ItemClickedHardverActivity.java
@@ -5,12 +5,20 @@ import hr.dominik.library.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class ItemClickedHardverActivity extends AppCompatActivity {
 
     TextView hardverKnjiga;
     TextView hardverPisac;
+    EditText editTextPosudbaImeHardver;
+    EditText editTextPosudbaPrezimeHardver;
+    EditText editTextPosudbaEmailHardver;
+    Button gumbPosudi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +28,10 @@ public class ItemClickedHardverActivity extends AppCompatActivity {
 
         hardverKnjiga = findViewById(R.id.hardverKnjiga);
         hardverPisac = findViewById(R.id.hardverPisac);
+        editTextPosudbaImeHardver = findViewById(R.id.editTextPosudbaImeHardver);
+        editTextPosudbaPrezimeHardver = findViewById(R.id.editTextPosudbaPrezimeHardver);
+        editTextPosudbaEmailHardver = findViewById(R.id.editTextPosudbaEmailHardver);
+        gumbPosudi = findViewById(R.id.gumbPosudi);
 
         Intent intent = getIntent();
         String name = intent.getStringExtra("name");
@@ -30,5 +42,18 @@ public class ItemClickedHardverActivity extends AppCompatActivity {
 
         hardverKnjiga.setText(name);
         hardverPisac.setText(author);
+
+        gumbPosudi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String editTextIme = editTextPosudbaImeHardver.getText().toString();
+                String editTextPrezime = editTextPosudbaPrezimeHardver.getText().toString();
+                String editTextEmail = editTextPosudbaEmailHardver.getText().toString();
+                Toast.makeText(ItemClickedHardverActivity.this,editTextIme + editTextPrezime
+                        + ". Zahvaljujemo se na posudbi.Detaljne podatke smo vam poslali na email "
+                        + editTextEmail + ". Srdačan Pozdrav!",Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 }
